ad22c89ca5b6fb6a8b9363c59b04306a5dd710fc
Valentina <raton13@rambler.ru>
2015-06-23 21:35:00 +0300

    2 les

---
+++ src/cycle.java
@@ -0,0 +1,33 @@
+/**
+ * Created by ITHILLEL6 on 23.06.2015.
+ */
+public class cycle {
+    public static void main(String[] args) {
+     /* int i = 0;
+        while(i<10){
+            i++; // ++i ; i++; --i: i--;
+            System.out.println(i);
+        }
+
+        //int i = 5;
+        //System.out.println(i+=2);
+        //System.out.println(i);
+*/
+     /*   int i = 0;
+        do{
+            System.out.println(i);
+            int q = 7
+        }while(i<30)
+        */
+
+       for(int i = 0;i<10;i++){
+           System.out.println(i);
+           continue; //brake;
+
+
+
+    }
+
+
+
+}
